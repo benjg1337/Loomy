2d8b9212cda5d896c11f42580d9a6503f2ae4040
benji <129755752+benjg1337@users.noreply.github.com>
2024-06-24 18:37:13 +0100

    Update loomy.sh

--- loomy.sh
+++ loomy.sh
@@ -84,11 +84,13 @@ read_input() {
 
 # Functions for each menu option
 system_info() {
+    clear
     uptime && grep MemTotal /proc/meminfo | awk '{print $2/1024/1024 " GB"}' 
     read -p "Press [Enter] key to return to main menu..."
 }
 
 show_crontab() {
+    clear
     crontab -l 2>&1
     read -p "Press [Enter] key to return to main menu..."
 }
@@ -99,58 +101,68 @@ netstat_output() {
 }
 
 kernel_version() {
+    clear
     uname -r 
     read -p "Press [Enter] key to return to main menu..."
 }
 
 distro_version() {
+    clear
     echo "Current distro being ran:"
     cat /etc/*release | grep PRETTY_NAME | cut -d "=" -f 2- | tr -d '"'
     read -p "Press [Enter] key to return to main menu..."
 }
 
 directory_changes() {
+    clear
     echo "Displaying free memory:"
     free -h
     read -p "Press [Enter] key to return to main menu..."
 }
 
 installed_packages() {
+    clear
     head -n 200 /var/log/apt/history.log 2>/dev/null | grep -oP 'Commandline: \K.*' ; head -n 200 /var/log/apt/yum.log 2>/dev/null | grep -oP 'Commandline: \K.*' ; head -n 200 /var/log/apt/dnf.log 2>/dev/null | grep -oP 'Commandline: \K.*' ; head -n 200 /var/log/apt/pacman.log 2>/dev/null | grep -oP 'Commandline: \K.*'
     read -p "Press [Enter] key to return to main menu..."
 }
 
 list_containers() {
+    clear
     echo "Displaying currently logged in users:"
     who
     read -p "Press [Enter] key to return to main menu..."
 }
 
 network_capture() {
+    clear
     echo "Displaying running processes:"
     ps aux
     read -p "Press [Enter] key to return to main menu..."
 }
 
 local_login() {
+    clear
     echo "Displaying last system boot:"
     who -b
     read -p "Press [Enter] key to return to main menu..."
 }
 
 ssh_auths() {
+    clear
     echo "Displaying kernel version:"
     uname -r
     read -p "Press [Enter] key to return to main menu..."
 }
 
 failed_ssh_auths() {
+    clear
     echo "Displaying hostname:"
     hostname
     read -p "Press [Enter] key to return to main menu..."
 }
 
 bash_history() {
+    clear
     tail -n 20 ~/.bash_history
     read -p "Press [Enter] key to return to main menu..."
 }
