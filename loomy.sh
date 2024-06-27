ef9ccdaf63e0b83f46c95cc03011e3a921a2c154
benji <129755752+benjg1337@users.noreply.github.com>
2024-06-27 16:01:28 +0100

    Update loomy.sh

--- loomy.sh
+++ loomy.sh
@@ -59,6 +59,7 @@ option5() { cat /etc/*release | grep PRETTY_NAME | cut -d "=" -f 2- | tr -d '"';
 option6() { find /home /opt -type f -mtime -7 -exec ls -l {} +; exit 0; }
 option7() { head -n 200 /var/log/apt/history.log 2>/dev/null | grep -oP 'Commandline: \K.*' ; head -n 200 /var/log/apt/yum.log 2>/dev/null | grep -oP 'Commandline: \K.*' ; head -n 200 /var/log/apt/dnf.log 2>/dev/null | grep -oP 'Commandline: \K.*' ; head -n 200 /var/log/apt/pacman.log 2>/dev/null | grep -oP 'Commandline: \K.*'; exit 0; }
 option8() {(command -v docker &>/dev/null && docker ps) || (command -v podman &>/dev/null && podman ps); exit 0; }
+option9() { }
 option10() { [ -z "$logins" ] && echo "No users have logged in within the last 7 days." || echo "$logins"; exit 0; }
 option11() { journalctl -u ssh | grep 'sshd.*Accepted password' | tail -n 20; exit 0; }
 option12() { journalctl -u ssh | grep 'sshd.*Failed password' | tail -n 20; exit 0; }
@@ -101,7 +102,7 @@ while [[ "$#" -gt 0 ]]; do
         -lv|--list-virtuals)
             option8
             ;;
-        -o9|--option9)
+        -nc|--network-cap)
             option9
             ;;
         -ll|--local-logins)
